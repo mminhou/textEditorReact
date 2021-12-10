5635f6eca790fb7bdcd716633ddfae5eba51904a
R _ R <exit19093@gmail.com>
2021-12-10 16:06:15 +0900

    Feat: 요구사항 만족하는 1차 완성본

--- src/component/Notepad.tsx
+++ src/component/Notepad.tsx
@@ -1,4 +1,4 @@
-import React, {useRef, useState} from 'react';
+import React, {useState} from 'react';
 import {AppBar, Button, Grid, Menu, MenuItem, TextField} from "@material-ui/core";
 import '../index.scss';
 
@@ -21,8 +21,8 @@ function Notepad({tabList, storageTabList, activatedTab}: NotepadProps) {
     const [_activatedTab, setActiveTab] = useState<Tab>(activatedTab);
 
     const [_editedContent, setEditedContent] = useState<string>('');
+    const [_editedTitle, setEditedTitle] = useState<string>('');
     const [anchorEl, setAnchorEl] = React.useState<HTMLAnchorElement>(null);
-    const inputRef = useRef<string>();
 
     /**
      * Tab 생성을 위한 로직
@@ -31,13 +31,13 @@ function Notepad({tabList, storageTabList, activatedTab}: NotepadProps) {
         console.log('Add new tab data .....');
         // 새로운 탭에 랜덤넘버 부여 -> 이미 가지고있는 탭과 동일하다면 err 발생!
         const cnt: number = Math.floor(Math.random() * 100);
-        const chk: Tab = _tabList.find(e => e.title === 'tab'+cnt);
+        const chk: Tab = _tabList.find(e => e.title === 'tab' + cnt);
         if (chk) {
             alert('Please retry!');
             return
         }
 
-        const newTab: Tab = {title: 'tab'+cnt, content: 'your code here.....', editedContent: '', isEdited: false};
+        const newTab: Tab = {title: 'tab' + cnt, content: 'your code here.....', editedContent: '', isEdited: true};
         setTabList([..._tabList, newTab]);
         setActiveTab(newTab);
     };
@@ -69,14 +69,17 @@ function Notepad({tabList, storageTabList, activatedTab}: NotepadProps) {
             return
         }
         const targetTab: Tab = _storageTabList.find(e => e.title === _activatedTab.title);
+        const targetTab2: Tab = _tabList.find(e => e.title === _activatedTab.title);
         // targetTab이 있으면 content만 변경, 없으면 storage에 새로운 tab data 추가
         if (targetTab) {
-            targetTab.content = _editedContent;
+            if (_editedContent) {
+                targetTab.content = _editedContent;
+            }
         } else {
             _storageTabList.push(_activatedTab);
         }
         // 이 부분을 수정해야 될 거같은디...
-        _activatedTab.isEdited = false;
+        targetTab2.isEdited = false;
         // 현재 state tabList에서 해당 tab의 indicator 제거하기 위함.
         setStorageTabList(_storageTabList);
         localStorage.setItem('tabList', JSON.stringify(_storageTabList));
@@ -92,12 +95,29 @@ function Notepad({tabList, storageTabList, activatedTab}: NotepadProps) {
             alert('Error: The selected tab does not exist.')
             return
         }
-        const targetTab = _storageTabList.find(e => e.title === _activatedTab.title);
-        if (targetTab) {
-            targetTab.title = ''
+        const storageTargetTab = _storageTabList.find(e => e.title === _activatedTab.title);
+        const targetTab =  _tabList.find(e => e.title === _activatedTab.title);
+
+        if (_storageTabList.find(e => e.title === _editedTitle) || _tabList.find(e => e.title === _editedTitle)) {
+            alert('Error: The title is already in use.')
+            return
+        }
+
+        if (storageTargetTab) {
+            if (_editedTitle) storageTargetTab.title = _editedTitle;
+            if (_editedContent) storageTargetTab.content = _editedContent;
         } else {
+            if (_editedTitle) targetTab.title = _editedTitle
+            if (_editedContent) targetTab.content = _editedContent
             _storageTabList.push(_activatedTab)
         }
+
+        targetTab.isEdited = false;
+        storageTargetTab.isEdited = false;
+
+        setStorageTabList(_storageTabList)
+        localStorage.setItem('tabList', JSON.stringify(_storageTabList));
+        alert('Successful save as another title.')
     };
 
 
@@ -125,15 +145,22 @@ function Notepad({tabList, storageTabList, activatedTab}: NotepadProps) {
      * Tab의 content가 수정되었는지 확인하기 위한 함수
      * -> onChange 메서드를 통해 event 타겟의 value가 발생하면 수정되었다고 판단 -> 수정된 tab의 title 색상을 바꿔준다.
      */
-    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setEditedContent(e.target.value);
+    const onChangeContent = (e: React.ChangeEvent<HTMLInputElement>) => {
+        console.log(e.target.value);
         // 최적화하기 위한 로직 -> 이미 edited 된 상태의 tab은 return 처리
+        _activatedTab.content = e.target.value;
+        setEditedContent(e.target.value);
         if (_activatedTab.isEdited) return
         // event 타겟의 value 가 발생시 수정했다고 판단 -> 활성화 된 tab의 edit flag를 바꿔준다.
-        if (e.target.value !== _activatedTab.content) {
-            const targetTab: Tab = _tabList.find(e => e.title === _activatedTab.title);
-            targetTab.isEdited = true;
-        }
+        const targetTab: Tab = _tabList.find(e => e.title === _activatedTab.title);
+        targetTab.isEdited = true;
+    }
+
+    /**
+     * Tab 의 title 수정을 위한 field가 채워질 때, editedTitle을 수정하기 위한 로직
+     */
+    const onChangeTitle = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEditedTitle(e.target.value);
     }
 
     /**
@@ -155,27 +182,36 @@ function Notepad({tabList, storageTabList, activatedTab}: NotepadProps) {
             <Grid item xs={12}>
                 <AppBar position="static" style={{backgroundColor: "rgb(43, 43, 43)"}}>
                     <div className="toolbar">
-                        <Button className="menu-list-item" onClick={add}>new tab</Button>
-                        <Button className="menu-list-item" onClick={handleMenuClick}>load</Button>
-                        <Menu id="simple-menu" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
+                        <Button className="menu-list-item" onClick={add}>
+                            new tab
+                        </Button>
+                        <Button className="menu-list-item" onClick={handleMenuClick}>
+                            load
+                        </Button>
+                        <Menu id="simple-menu"
+                              anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
                             {_storageTabList.map((tab, i: number) => (
-                                <MenuItem key={i} style={{width: 150}} onClick={() => load(tab)}>* {tab.title}</MenuItem>
+                                <MenuItem key={i} style={{width: 150}}
+                                          onClick={() => load(tab)}>* {tab.title}</MenuItem>
                             ))}
                         </Menu>
-                        <Button className="menu-list-item" onClick={save}>save</Button>
-                        <Button className="menu-list-item" onClick={saveAs}>save as</Button>
+                        <Button className="menu-list-item" onClick={save}>
+                            save
+                        </Button>
+                        <Button className="menu-list-item" onClick={saveAs}>
+                            save as</Button>
                         <TextField size="small" id="outlined-size-normal" placeholder="another title"
-                                   variant="outlined">
+                                   variant="outlined"
+                                   onChange={onChangeTitle}>
                         </TextField>
                     </div>
                     <div style={{display: "inline-block", padding: 10}}>
                         {_tabList.map((tab, i: number) => (
-                            <div key={i} className="tab-list-item"
-                                 style={{width: 100 / _tabList.length + '%', float: "left"}}>
+                            <div key={i} className="tab-list-item" style={{width: 100 / _tabList.length + '%', float: "left"}}>
                                 <Button className="tab-list-title-button" onClick={() => activate(tab)}
                                         style={{
                                             backgroundColor: _activatedTab && _activatedTab.title === tab.title ? 'rgb(116, 122, 128)' : 'rgb(43, 43, 43)',
-                                            color: tab.isEdited ? 'rgb(209, 103, 90)' : 'white'
+                                            color: tab.isEdited ? 'rgb(209, 103, 90)' : 'white',
                                         }}>
                                     {tab.title}
                                 </Button>
@@ -193,7 +229,7 @@ function Notepad({tabList, storageTabList, activatedTab}: NotepadProps) {
                         <TextField key={i} className="tab-content"
                                    multiline rows={50} variant="filled"
                                    defaultValue={tab.content}
-                                   onChange={onChange}
+                                   onChange={onChangeContent}
                         /> : ''
                 ))}
             </Grid>
@@ -205,7 +241,7 @@ function Notepad({tabList, storageTabList, activatedTab}: NotepadProps) {
 
 Notepad.defaultProps = {
     tabList: [],
-    storageTabList: JSON.parse(localStorage.getItem('tabList')),
+    storageTabList: JSON.parse(localStorage.getItem('tabList')) || [],
     activatedTab: null,
 };
 
