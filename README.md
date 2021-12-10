9a3e67bb6052287254eebfc66a537db6989a33da
R _ R <exit19093@gmail.com>
2021-12-10 14:20:08 +0900

    Fix: material ui <-> domfindele 충돌되는 문제 theme 삽입으로 해결

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # textEditorReact
 
 ## vanillaJS -> React 로 구현하기  
+
+### 개선할 것
+1. save 버튼 클릭 후 tab indicator 비활성화 이슈
+   - 현재 save 버튼 클릭 후 tab의 indicator 비활성화가 안됨
+   - 다른 탭으로 이동해야 indicator가 사라짐. 
--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import './index.scss';
 import App from "./App";
+import {ThemeProvider} from "@material-ui/core/styles";
+import {unstable_createMuiStrictModeTheme} from '@material-ui/core/styles';
+
+// findDomElement Error를 잡기위한 코드 -> material ui provider로 감싸기.
+const theme = unstable_createMuiStrictModeTheme();
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
+    <ThemeProvider theme={theme}>
+        <React.StrictMode>
+            <App/>
+        </React.StrictMode>
+    </ThemeProvider>,
+
     document.getElementById("root")
 );
