eb82613a69c079f7c842f8ec61edacf74798c47f
R _ R <exit19093@gmail.com>
2021-12-14 11:52:07 +0900

    Chore: login인 한 사용자만이 탭화면 볼 수 있도록 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Notepad from './component/Notepad';
+import { auth } from "./firebase";
+import GoogleSignIn from "./component/GoogleSignIn";
 
 function App() {
+    const [user, setUser] = useState<any>();
+    auth.onAuthStateChanged(user => {
+        // user.currentUser 를 통해 현재 로그인 중인 사용자에 대한 정보를 이용할 수 있다.
+        // ex) user.currentUser.email, user.currentUser.displayName ... etc
+
+        if (user !== null) {
+            setUser(user);
+        }
+    });
+
     return (
         <>
-            <Notepad />
+            {user ?
+                <Notepad />
+                :
+                <GoogleSignIn/>
+            }
         </>
     )
 }
