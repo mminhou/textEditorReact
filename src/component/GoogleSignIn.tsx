9216c4d393e024a8295ae2594948c8307741c1ed
R _ R <exit19093@gmail.com>
2021-12-14 11:51:30 +0900

    Feat: google login component 추가

---
+++ src/component/GoogleSignIn.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {auth, signInWithGoogle} from "../firebase";
+import {Grid} from "@material-ui/core";
+
+function GoogleSignIn() {
+    auth.onAuthStateChanged(user => {
+        // user.currentUser 를 통해 현재 로그인 중인 사용자에 대한 정보를 이용할 수 있다.
+        // ex) user.currentUser.email, user.currentUser.displayName ... etc
+
+        if (user !== null) {
+            console.log(user)
+            console.log("로그인 되었습니다.")
+        }
+    });
+
+    return (
+        <Grid container style={{height: '100%'}} justifyContent="center" alignItems="center">
+            <Grid item xs={12}>
+                <button type="button" className="login-with-google-btn" onClick={signInWithGoogle}>
+                    Sign in with Google
+                </button>
+            </Grid>
+        </Grid>
+
+    );
+}
+
+export default GoogleSignIn;
