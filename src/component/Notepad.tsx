ca27b50c2dac798300ea492af3c0645c42c5af88
R _ R <exit19093@gmail.com>
2021-12-14 17:33:32 +0900

    Feat: localstorage -> firebase storage 고도화 작업 진행중

--- src/component/Notepad.tsx
+++ src/component/Notepad.tsx
@@ -1,5 +1,5 @@
-import React, {useState} from 'react';
-import {AppBar, Button, Grid, Menu, MenuItem, TextField} from "@material-ui/core";
+import React, {useEffect, useState} from 'react';
+import {AppBar, Grid} from "@material-ui/core";
 import '../index.scss';
 import {NotepadProps, Tab} from "./comm/Comm";
 import Content from "./Content";
@@ -9,14 +9,130 @@ import SaveAsButton from "./button/SaveAsButton";
 import AddButton from "./button/AddButton";
 import SaveButton from "./button/SaveButton";
 import LoadButton from "./button/LoadButton";
+import {fireStorage} from "../firebase";
+import axios from "axios";
+
+async function getFirebaseStorageData() {
+    const storageRef = fireStorage.ref().child('data.json');
+    let url = await storageRef.getDownloadURL();
+    let res = await axios.get(url);
+    return res.data.tabList
+
+    // const storageRef = fireStorage.ref().child('data.json');
+    // const fr = new FileReader();
+    // let res;
+    //
+    // storageRef.getDownloadURL()
+    //     .then(function (url) {
+    //         const xhr = new XMLHttpRequest();
+    //         xhr.responseType = "blob";
+    //         xhr.onload = function (event) {
+    //             const blob = xhr.response;
+    //             fr.readAsText(blob);
+    //         };
+    //         xhr.open("GET", url);
+    //         xhr.send();
+    //
+    //     })
+    //     .catch(function (error) {
+    //         // Handle any errors
+    //     });
+    //
+    // fr.addEventListener("load", (e) => {
+    //     const restored = JSON.parse(fr.result);
+    // });
+}
+
 
 function Notepad({tabList, storageTabList, activatedTab}: NotepadProps) {
     const [_tabList, setTabList] = useState<Tab[]>(tabList);
     const [_storageTabList, setStorageTabList] = useState<Tab[]>(storageTabList);
     const [_activatedTab, setActivatedTab] = useState<Tab>(activatedTab);
-
     const [_editedTitle, setEditedTitle] = useState<string>('');
     const [_editedContent, setEditedContent] = useState<string>('');
+
+    console.log(_storageTabList);
+
+    useEffect(async () => {
+        const res = await getFirebaseStorageData();
+        setStorageTabList(res);
+    }, [])
+
+
+    const storageRef = fireStorage.ref().child('data.json');
+    // const res = storageRef.child('data.json');
+
+    // json 데이터 저장하는 방법
+    const backupData = {
+        tabList: [
+            {
+                "title": "tab1",
+                "content": "tab1 Contenta asfsadf",
+                "editedContent": '',
+                "isEdited": false
+            },
+            {
+                "title": "tab2",
+                "content": "tab2 123123123123",
+                "editedContent": '',
+                "isEdited": false
+            },
+            {
+                "title": "tab3",
+                "content": "tab3 @!#!@#@!#@!#@!#",
+                "editedContent": '',
+                "isEdited": false
+            },
+        ]
+    };
+    backupData.tabList.push({
+        "title": "tab4",
+        "content": "tab4 오마이갓 너무 졸려",
+        "editedContent": '',
+        "isEdited": false
+    })
+
+    const jsonString = JSON.stringify(backupData);
+    const blob = new Blob([jsonString], {type: "application/json"});
+    storageRef.put(blob).then(() => {
+        console.log("success upload backup");
+    });
+
+    // json 데이터 가져오기
+
+
+    // storageRef.listAll().then(function(res) {
+    //     console.log(res);
+    // }).catch(
+    //     // Err handling
+    // )
+
+    // function getFirebaseStorageData() {
+    //     const storageRef = fireStorage.ref().child('data.json');
+    //     const fr = new FileReader();
+    //     let res;
+    //
+    //     storageRef.getDownloadURL()
+    //         .then(function (url) {
+    //             const xhr = new XMLHttpRequest();
+    //             xhr.responseType = "blob";
+    //             xhr.onload = function (event) {
+    //                 const blob = xhr.response;
+    //                 fr.readAsText(blob);
+    //             };
+    //             xhr.open("GET", url);
+    //             xhr.send();
+    //
+    //         })
+    //         .catch(function (error) {
+    //             // Handle any errors
+    //         });
+    //
+    //     fr.addEventListener("load", (e) => {
+    //         const restored = JSON.parse(fr.result);
+    //         setStorageTabList(restored);
+    //     });
+    // }
 
 
     /**
@@ -29,7 +145,10 @@ function Notepad({tabList, storageTabList, activatedTab}: NotepadProps) {
             targetTab.content = tab.content;
             targetTab.isEdited = false;
         } else {
+            console.log(targetTab);
             console.log(_storageTabList);
+            console.log(tab, '-----------------');
+            // setStorageTabList([..._storageTabList, tab]);
             setStorageTabList([..._storageTabList, tab]);
             console.log(_storageTabList);
             // _storageTabList.push(tab);
@@ -45,23 +164,29 @@ function Notepad({tabList, storageTabList, activatedTab}: NotepadProps) {
             <Grid item xs={12}>
                 <AppBar position="static" style={{backgroundColor: "rgb(43, 43, 43)"}}>
                     <div className="toolbar">
-                        <AddButton _tabList={_tabList} _activatedTab={_activatedTab} setTabList={setTabList} setActivatedTab={setActivatedTab}/>
-                        <LoadButton _tabList={_tabList} _storageTabList={_storageTabList} setActivatedTab={setActivatedTab} setTabList={setTabList} />
-                        <SaveButton _tabList={_tabList} _activatedTab={_activatedTab} _editedContent={_editedContent} saveStorage={saveStorage} />
-                        <SaveAsButton _tabList={_tabList} _storageTabList={_storageTabList} _activatedTab={_activatedTab} _editedTitle={_editedTitle} _editedContent={_editedContent} saveStorage={saveStorage} />
-                        <Title setEditedTitle={setEditedTitle} />
+                        <AddButton _tabList={_tabList} _activatedTab={_activatedTab} setTabList={setTabList}
+                                   setActivatedTab={setActivatedTab}/>
+                        <LoadButton _tabList={_tabList} _storageTabList={_storageTabList}
+                                    setActivatedTab={setActivatedTab} setTabList={setTabList}/>
+                        <SaveButton _tabList={_tabList} _activatedTab={_activatedTab} _editedContent={_editedContent}
+                                    saveStorage={saveStorage}/>
+                        <SaveAsButton _tabList={_tabList} _storageTabList={_storageTabList}
+                                      _activatedTab={_activatedTab} _editedTitle={_editedTitle}
+                                      _editedContent={_editedContent} saveStorage={saveStorage}/>
+                        <Title setEditedTitle={setEditedTitle}/>
                     </div>
-                    <TabList _tabList={_tabList} _activatedTab={_activatedTab} setTabList={setTabList} setActivatedTab={setActivatedTab} />
+                    <TabList _tabList={_tabList} _activatedTab={_activatedTab} setTabList={setTabList}
+                             setActivatedTab={setActivatedTab}/>
                 </AppBar>
             </Grid>
-            <Content _tabList={_tabList} _activatedTab={_activatedTab} setEditedContent={setEditedContent} />
+            <Content _tabList={_tabList} _activatedTab={_activatedTab} setEditedContent={setEditedContent}/>
         </Grid>
     )
 };
 
 Notepad.defaultProps = {
     tabList: [],
-    storageTabList: JSON.parse(localStorage.getItem('tabList')) || [],
+    storageTabList: [],
     activatedTab: null,
 };
 
